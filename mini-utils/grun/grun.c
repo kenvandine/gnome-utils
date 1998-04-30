566fdf7f76132b5e8dd81963d053b94a86cc43de
Havoc Pennington <hp@pobox.com>
1998-04-30 08:04:55 +0000

    I hope this isn't too bloated now - the binary is almost 6K! :-)
    I hope this isn't too bloated now - the binary is almost 6K! :-)
    
    Thu Apr 30 03:03:58 1998  Havoc Pennington  <hp@pobox.com>
    
    	* grun.c: Gnomified, uses a whole dialog instead of just an entry.
    	Took out broken accelerator table stuff. Once GnomeDialog's
    	accelerators are fixed the key_press_event stuff can also come
    	out.

--- mini-utils/grun/grun.c
+++ mini-utils/grun/grun.c
@@ -3,7 +3,7 @@
    wrong with GtkAccelerator?!?!
  */
 #include <unistd.h>
-#include <gtk/gtk.h>
+#include <gnome.h>
 #include <gdk/gdkkeysyms.h>
 
 GtkWidget *thewin;
@@ -30,38 +30,36 @@ void dorun(GtkWidget *widget, GtkWidget *theent)
 	gtk_main_quit();
 }
 
+#define APPNAME "grun"
+
 int
 main(int argc, char *argv[])
 {
 	GtkWidget *theent;
-	GtkAcceleratorTable *accel;
 
-	gtk_init(&argc, &argv);
+	gnome_init (APPNAME, 0, argc, argv, 0, 0);
 
-	thewin = gtk_window_new(GTK_WINDOW_DIALOG);
-	gtk_window_set_title(GTK_WINDOW(thewin), "Run program");
+	thewin = gnome_dialog_new(_("Run a Program"),
+				  GNOME_STOCK_BUTTON_OK,
+				  GNOME_STOCK_BUTTON_CANCEL, NULL);
 	gtk_window_position(GTK_WINDOW(thewin), GTK_WIN_POS_CENTER);
 	gtk_window_set_policy(GTK_WINDOW(thewin), FALSE, FALSE, TRUE);
 	gtk_signal_connect(GTK_OBJECT(thewin), "delete_event",
 			   GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
 
+	gnome_dialog_button_connect(GNOME_DIALOG(thewin), 0, 
+				    GTK_SIGNAL_FUNC(dorun), theent);
+	gnome_dialog_button_connect(GNOME_DIALOG(thewin), 1, 
+				    GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
+
 	theent = gtk_entry_new();
-	gtk_container_add(GTK_CONTAINER(thewin), theent);
+	gtk_container_add(GTK_CONTAINER(GNOME_DIALOG(thewin)->vbox), theent);
 	gtk_signal_connect(GTK_OBJECT(theent), "key_press_event",
 			   GTK_SIGNAL_FUNC(dokey), NULL);
 	gtk_signal_connect(GTK_OBJECT(theent), "activate",
 			   GTK_SIGNAL_FUNC(dorun), theent);
 	gtk_window_set_focus(GTK_WINDOW(thewin), theent);
 
-	accel = gtk_accelerator_table_new();
-#if 0
-	gtk_widget_install_accelerator (thewin, accel,
-				"delete_event",
-				GDK_Escape, 0);
-	gtk_widget_install_accelerator (theent, accel,
-				"activate",
-				GDK_Escape, 0);
-#endif
 	gtk_widget_show_all(thewin);
 
 	gtk_main();
