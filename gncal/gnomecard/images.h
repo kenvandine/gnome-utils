e69694a106ccf93411f88d1fc430cb0e20efcf29
Arturo Espinosa <unammx@src.gnome.org>
1998-08-04 01:48:03 +0000

    Forgot to add these new files.
    Arturo

---
+++ gncal/gnomecard/images.h
@@ -0,0 +1,19 @@
+#include "world.xpm"
+
+/* Stock pixmaps */
+
+#include "cardnew.xpm"
+#include "cardedit.xpm"
+#include "first.xpm"
+#include "last.xpm"
+
+/* Tree icons */
+
+#include "ident.xpm"
+#include "geo.xpm"
+#include "org.xpm"
+#include "expl.xpm"
+#include "sec.xpm"
+#include "phone.xpm"
+#include "email.xpm"
+#include "addr.xpm"
