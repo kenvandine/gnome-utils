ec0b498f5fe4d18b8c864f9a9ee6d82770b25e07
Havoc Pennington <hp@src.gnome.org>
1998-09-21 03:12:46 +0000

    Removed everything here except wmconfig2desktop, since edit-menus has been replaced by gmenu. Someone might want to put wmconfig2desktop somewhere better.

--- edit-menus/icewm.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef ICEWM_H
-#define ICEWM_H
-
-#define RESTART_NOT_PROG "restart-not-prog"
-#define ICEWM_TOPLEVEL_MENU "Icewm Start Menu"
-
-#endif
--- edit-menus/is_image.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef IS_IMAGE_H
-#define IS_IMAGE_H
-
-#include <glib.h>
-
-gboolean is_image_file(const gchar * filename);
-
-#endif
