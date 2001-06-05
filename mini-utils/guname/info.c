2e2c47d50e43f14263a7aa2d66abe963db72258a
Kjartan Maraas <kmaraas@gnome.org>
2001-06-05 18:21:07 +0000

    Apply patch to remove commented out code from Red Hat Linux.
    2001-06-05  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* info.c: Apply patch to remove commented out code from Red Hat Linux.

--- mini-utils/guname/info.c
+++ mini-utils/guname/info.c
@@ -179,12 +179,7 @@ static void get_linux_info()
     f = fopen("/etc/redhat-release", "r");
     if (f) {
       fgets(buf, 79, f);
-/* drmike - this seems unecessary in RH 5.2 and later
-      if (strchr(buf, ' '))
-        info[si_distribution_version] = g_strdup(strchr(buf, ' ')+1);
-      else
-*/
-        info[si_distribution_version] = g_strdup(buf);
+      info[si_distribution_version] = g_strdup(buf);
       fclose(f);
     }
   } else if (g_file_exists("/etc/SuSE-release")) {
