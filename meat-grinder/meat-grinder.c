edb4e00d3f6ccad128819cafca88b1bb43c2ed13
Kevin Vandersloot <kfv101@psu.edu>
2002-04-17 02:00:39 +0000

    make help work. From Satyajit Kanungo - bug #78745
    2002-04-16  Kevin Vandersloot <kfv101@psu.edu>
    
            * Makefile.am, meat-grinder.c: make help work. From
            Satyajit Kanungo  - bug #78745

--- meat-grinder/meat-grinder.c
+++ meat-grinder/meat-grinder.c
@@ -919,9 +919,9 @@ main (gint argc, gchar *argv [])
 	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
 	textdomain(GETTEXT_PACKAGE);
 
-	gnome_program_init ("archive-generator", VERSION, LIBGNOMEUI_MODULE,
+	gnome_program_init ("meat-grinder", VERSION, LIBGNOMEUI_MODULE,
 				    argc, argv, GNOME_PARAM_POPT_TABLE,
-				    &options, NULL);
+				    &options,GNOME_PARAM_APP_DATADIR,DATADIR,NULL);
 
 	client = gnome_master_client ();
 
