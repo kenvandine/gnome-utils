f593868a3fac5914890cea6135baa495d86d1bc8
Dennis M. Cranston <dennis_cranston@yahoo.com>
2002-10-07 03:51:43 +0000

    Updated the no viewer available error message dialog.
    2002-10-06  Dennis M. Cranston <dennis_cranston@yahoo.com>
    
    	* gsearchtool-callbacks.c:  Updated the no viewer
    	available error message dialog.

--- gsearchtool/gsearchtool-callbacks.c
+++ gsearchtool/gsearchtool-callbacks.c
@@ -290,8 +290,22 @@ open_file_cb (GtkWidget 	*widget,
 		}
 		else {
 			if (!open_file_with_application (locale_file)) {
-				gnome_error_dialog_parented (_("Sorry, no viewer available for this file type."),
-						    	     GTK_WINDOW(interface.main_window));
+				GtkWidget *dialog;
+
+                		dialog = gtk_message_dialog_new (GTK_WINDOW (interface.main_window),
+                        		GTK_DIALOG_DESTROY_WITH_PARENT,
+                        		GTK_MESSAGE_ERROR,
+                        		GTK_BUTTONS_OK,
+                        		_("There is no installed viewer capable of displaying \"%s\"."),
+					file);
+
+                		g_signal_connect (G_OBJECT (dialog),
+                        		"response",
+                        		G_CALLBACK (gtk_widget_destroy), NULL);
+				
+       	         		gtk_window_set_title (GTK_WINDOW (dialog), _("Can't Display Location"));
+       	         		gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
+	                	gtk_widget_show (dialog);
 			}
 		} 
 		g_free (file);
@@ -389,8 +403,21 @@ click_file_cb 	     (GtkWidget 	*widget,
 			}
 			else {
 				if (!open_file_with_application (locale_file)) {
-					gnome_error_dialog_parented (_("Sorry, no viewer available for this file type."),
-							    	     GTK_WINDOW(interface.main_window));
+					GtkWidget *dialog;
+
+                			dialog = gtk_message_dialog_new (GTK_WINDOW (interface.main_window),
+                        			GTK_DIALOG_DESTROY_WITH_PARENT,
+                        			GTK_MESSAGE_ERROR,
+                        			GTK_BUTTONS_OK,
+                        			_("There is no installed viewer capable of displaying \"%s\"."),
+						file);
+					
+					g_signal_connect (G_OBJECT (dialog),
+                        			"response",
+                        			G_CALLBACK (gtk_widget_destroy), NULL);
+					gtk_window_set_title (GTK_WINDOW (dialog), _("Can't Display Location"));
+       	         			gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
+	                		gtk_widget_show (dialog);
 				}
 			} 
 			g_free (file);
