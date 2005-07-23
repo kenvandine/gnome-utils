f395f549f763e95007cbf85da87425e6230b31ac
Dennis Cranston <dennis_cranston@yahoo.com>
2005-07-23 08:32:09 +0000

    Protect against thumbnail_pixbuf == NULL warnings.
    2005-07-23  Dennis Cranston <dennis_cranston@yahoo.com>
    
    	* gsearchtool-support.c: (get_file_pixbuf):  Protect against
    	thumbnail_pixbuf == NULL warnings.

--- gsearchtool/gsearchtool-support.c
+++ gsearchtool/gsearchtool-support.c
@@ -1134,39 +1134,42 @@ get_file_pixbuf (GSearchWindow * gsearch,
 			thumbnail_pixbuf = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (), icon_name, 
 			                                             ICON_SIZE, 0, NULL);
 
-			/*  The following is workaround for bugzilla report #311318,                     */
-			/*  "gtk_icon_theme_load_icon () can return pixbufs larger than requested size". */
-			/*  Please see the url, http://bugzilla.gnome.org/show_bug.cgi?id=311318.        */
-										     
-			if ((gdk_pixbuf_get_width (thumbnail_pixbuf) > ICON_SIZE) || 
-			    (gdk_pixbuf_get_height (thumbnail_pixbuf) > ICON_SIZE)) {
+			if (thumbnail_pixbuf != NULL) {
+			
+				/*  The following is a workaround for bugzilla report #311318,                   */
+				/*  "gtk_icon_theme_load_icon () can return pixbufs larger than requested size". */
+				/*  Please see the url, http://bugzilla.gnome.org/show_bug.cgi?id=311318.        */
+			
+				if ((gdk_pixbuf_get_width (thumbnail_pixbuf) > ICON_SIZE) || 
+				    (gdk_pixbuf_get_height (thumbnail_pixbuf) > ICON_SIZE)) {
 
-				gfloat scale_factor_x = 1.0;
-				gfloat scale_factor_y = 1.0;
-				gint scale_x;
-				gint scale_y;
+					gfloat scale_factor_x = 1.0;
+					gfloat scale_factor_y = 1.0;
+					gint scale_x;
+					gint scale_y;
 
-				if (gdk_pixbuf_get_width (thumbnail_pixbuf) > ICON_SIZE) {
-					scale_factor_x = (gfloat) ICON_SIZE / (gfloat) gdk_pixbuf_get_width (thumbnail_pixbuf);
-				}
-				if (gdk_pixbuf_get_height (thumbnail_pixbuf) > ICON_SIZE) {
-					scale_factor_y = (gfloat) ICON_SIZE / (gfloat) gdk_pixbuf_get_height (thumbnail_pixbuf);
-				}
+					if (gdk_pixbuf_get_width (thumbnail_pixbuf) > ICON_SIZE) {
+						scale_factor_x = (gfloat) ICON_SIZE / (gfloat) gdk_pixbuf_get_width (thumbnail_pixbuf);
+					}
+					if (gdk_pixbuf_get_height (thumbnail_pixbuf) > ICON_SIZE) {
+						scale_factor_y = (gfloat) ICON_SIZE / (gfloat) gdk_pixbuf_get_height (thumbnail_pixbuf);
+					}
 				
-				if (gdk_pixbuf_get_width (thumbnail_pixbuf) > gdk_pixbuf_get_height (thumbnail_pixbuf)) {
-					scale_x = ICON_SIZE;
-					scale_y = (gint) (gdk_pixbuf_get_height (thumbnail_pixbuf) * scale_factor_x);
+					if (gdk_pixbuf_get_width (thumbnail_pixbuf) > gdk_pixbuf_get_height (thumbnail_pixbuf)) {
+						scale_x = ICON_SIZE;
+						scale_y = (gint) (gdk_pixbuf_get_height (thumbnail_pixbuf) * scale_factor_x);
+					}
+					else {
+						scale_x = (gint) (gdk_pixbuf_get_width (thumbnail_pixbuf) * scale_factor_y);
+						scale_y = ICON_SIZE;
+					}    
+			    
+					pixbuf = gdk_pixbuf_scale_simple (thumbnail_pixbuf, scale_x, scale_y, GDK_INTERP_BILINEAR);
+					g_object_unref (thumbnail_pixbuf);
 				}
 				else {
-					scale_x = (gint) (gdk_pixbuf_get_width (thumbnail_pixbuf) * scale_factor_y);
-					scale_y = ICON_SIZE;
-				}    
-			    
-				pixbuf = gdk_pixbuf_scale_simple (thumbnail_pixbuf, scale_x, scale_y, GDK_INTERP_BILINEAR);
-				g_object_unref (thumbnail_pixbuf);
-			}
-			else {
-				pixbuf = thumbnail_pixbuf;
+					pixbuf = thumbnail_pixbuf;
+				}
 			}
 		}
 		
