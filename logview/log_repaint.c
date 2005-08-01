824b54ad49db41af6cf0d93b553743636d628359
Vincent Noel <vnoel@cox.net>
2005-08-01 13:58:57 +0000

    Don't crash on amd64 arch. Patch from Joe Marcus Clarke.
    2005-08-01  Vincent Noel  <vnoel@cox.net>
    
    	* log_repaint.c (logview_draw_log_lines): Don't crash on amd64
    	arch. Patch from Joe Marcus Clarke.

--- logview/log_repaint.c
+++ logview/log_repaint.c
@@ -413,8 +413,7 @@ logview_draw_log_lines (LogviewWindow *window, Log *current_log)
                 utf8 = GetDateHeader (line);
                 gtk_tree_store_append (GTK_TREE_STORE (model), &iter, NULL);
                 gtk_tree_store_set (GTK_TREE_STORE (model), &iter,
-                                    DATE, utf8, HOSTNAME, FALSE,
-                                    PROCESS, FALSE, MESSAGE, FALSE, -1);
+                                    DATE, utf8, -1);
                 
                 /* store pointer to the date headers, using the month and day as the key */
                 
