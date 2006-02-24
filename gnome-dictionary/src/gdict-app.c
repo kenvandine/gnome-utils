6b1ae1681540d35e1da145f4a2f674d81c4d974a
Emmanuele Bassi <ebassi@cvs.gnome.org>
2006-02-24 13:20:16 +0000

    remove the big frickin' dialog, and just rename the file that's causing
    2006-02-24  Emmanuele Bassi  <ebassi@cvs.gnome.org>
    
    	* src/gdict-app.c (gdict_app_create_data_dir): remove the big
    	frickin' dialog, and just rename the file that's causing the error
    	by appending the '.pre-2-14' string to its name.  Let's hope this
    	is enough.

--- gnome-dictionary/src/gdict-app.c
+++ gnome-dictionary/src/gdict-app.c
@@ -317,110 +317,48 @@ gdict_look_up_word_and_quit (GdictApp *app)
 static gboolean
 gdict_app_create_data_dir ()
 {
-  gchar *data_dir;
+  gchar *data_dir_name;
   
-  data_dir = g_build_filename (g_get_home_dir (),
-  			       ".gnome2",
-  			       "gnome-dictionary",
-  			       NULL);
+  data_dir_name = g_build_filename (g_get_home_dir (),
+  				    ".gnome2",
+  				    "gnome-dictionary",
+  				    NULL);
   
-  if (g_mkdir (data_dir, 0700) == -1)
+  if (g_mkdir (data_dir_name, 0700) == -1)
     {
       /* this is weird, but sometimes there's a "gnome-dictionary" file
        * inside $HOME/.gnome2; see bug #329126.
        */
       if ((errno == EEXIST) &&
-	  (g_file_test (data_dir, G_FILE_TEST_IS_REGULAR)))
+          (g_file_test (data_dir_name, G_FILE_TEST_IS_REGULAR)))
         {
-	  gchar *message;
-	  gchar *detail;
-	  GtkWidget *dialog;
-	  gint response;
-
-	  message = g_strdup_printf (_("Unable to create Dictionary data directory '%s'"), data_dir);
-	  detail = g_strdup(_("There already is a file with the same name.\n"
-			      "\n"
-			      "If you did not create the file yourself, you can safely remove it. "
-			      "If you want to move the file and check its contents later, you can make a copy. "
-			      "If you are not sure, you should close this dialog, check the contents "
-			      "of the file and then remove it manually before launching Dictionary again."));
-
-	  dialog = gtk_message_dialog_new (NULL,
-			  		   GTK_DIALOG_MODAL,
-					   GTK_MESSAGE_ERROR,
-					   GTK_BUTTONS_NONE,
-					   "%s", message);
-	  gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (dialog),
-			  			    "%s", detail);
-	  gtk_dialog_add_buttons (GTK_DIALOG (dialog),
-			  	  _("Remove the file"), 1,
-				  _("Make a copy"), 2,
-				  GTK_STOCK_CLOSE, GTK_RESPONSE_CLOSE,
-				  NULL);
-	  
-	  response = gtk_dialog_run (GTK_DIALOG (dialog));
-	  switch (response)
-            {
-            case 1:
-              /* the user pressed "delete" */
-              if (g_unlink (data_dir) == -1)
-	        {
-                  GtkWidget *error_dialog;
-
-		  error_dialog = gtk_message_dialog_new (NULL,
-							 GTK_DIALOG_MODAL,
-							 GTK_MESSAGE_ERROR,
-							 GTK_BUTTONS_CLOSE,
-							 _("Unable to remove file '%s': %s"),
-							 data_dir,
-							 g_strerror (errno));
-		  
-		  gtk_dialog_run (GTK_DIALOG (dialog));
-		  
-		  gtk_widget_destroy (dialog);
-		  g_free (data_dir);
-		  
-                  return FALSE;
-                }
-	      break;
-	    case 2:
-	      {
-              gchar *backup = g_strdup_printf ("%s.pre-2-14", data_dir);
+          gchar *backup = g_strdup_printf ("%s.pre-2-14", data_dir_name);
 	      
-	      /* the user pressed rename */
-	      if (g_rename (data_dir, backup) == -1)
-	        {
-                  GtkWidget *error_dialog;
+	  if (g_rename (data_dir_name, backup) == -1)
+	    {
+              GtkWidget *error_dialog;
 
-		  error_dialog = gtk_message_dialog_new (NULL,
-							 GTK_DIALOG_MODAL,
-							 GTK_MESSAGE_ERROR,
-							 GTK_BUTTONS_CLOSE,
-							 _("Unable to rename file '%s' to '%s': %s"),
-							 data_dir,
-							 backup,
-							 g_strerror (errno));
-		  
-		  gtk_dialog_run (GTK_DIALOG (dialog));
-		  
-		  gtk_widget_destroy (dialog);
-		  g_free (backup);
-		  g_free (data_dir);
-		  
-                  return FALSE;
-                }
-
+	      error_dialog = gtk_message_dialog_new (NULL,
+                                                     GTK_DIALOG_MODAL,
+						     GTK_MESSAGE_ERROR,
+						     GTK_BUTTONS_CLOSE,
+						     _("Unable to rename file '%s' to '%s': %s"),
+						     data_dir_name,
+						     backup,
+						     g_strerror (errno));
+	      
+	      gtk_dialog_run (GTK_DIALOG (error_dialog));
+	      
+	      gtk_widget_destroy (error_dialog);
 	      g_free (backup);
-	      }
-	      break;
-            default:
-              /* the user closed the dialog */
-              g_free (data_dir);
+	      g_free (data_dir_name);
 
 	      return FALSE;
-	    }
+            }
+
+	  g_free (backup);
 	  
-          if (g_mkdir (data_dir, 0700) == -1)
+          if (g_mkdir (data_dir_name, 0700) == -1)
             {
               GtkWidget *error_dialog;
 	      
@@ -429,20 +367,16 @@ gdict_app_create_data_dir ()
 						     GTK_MESSAGE_ERROR,
 						     GTK_BUTTONS_CLOSE,
 						     _("Unable to create the data directory '%s': %s"),
-						     data_dir,
+						     data_dir_name,
 						     g_strerror (errno));
 	      
 	      gtk_dialog_run (GTK_DIALOG (error_dialog));
 	      
 	      gtk_widget_destroy (error_dialog);
-              g_free (data_dir);
+              g_free (data_dir_name);
 
 	      return FALSE;
             }
-
-	  gtk_widget_destroy (dialog);
-	  g_free (detail);
-	  g_free (message);
 
 	  goto success;
 	}
@@ -456,20 +390,20 @@ gdict_app_create_data_dir ()
 						 GTK_MESSAGE_ERROR,
 						 GTK_BUTTONS_CLOSE,
 						 _("Unable to create the data directory '%s': %s"),
-						 data_dir,
+						 data_dir_name,
 						 g_strerror (errno));
 	  
 	  gtk_dialog_run (GTK_DIALOG (error_dialog));
 	  
 	  gtk_widget_destroy (error_dialog);
-	  g_free (data_dir);
+	  g_free (data_dir_name);
 
 	  return FALSE;
 	}
     }
 
 success:
-  g_free (data_dir);
+  g_free (data_dir_name);
 
   return TRUE;
 }
