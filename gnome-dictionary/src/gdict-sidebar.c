603b3995bd52a325fdac6d8dc7690aca1db59906
Emmanuele Bassi <ebassi@src.gnome.org>
2007-09-06 17:05:37 +0000

    Relicense GdictSidebar under the GNU LGPL v2.1 (or later)
    GdictSidebar should be released under the LGPL instead of the GPL, so
    that other projects can safely use it.
    
    svn path=/trunk/; revision=7675

--- gnome-dictionary/src/gdict-sidebar.c
+++ gnome-dictionary/src/gdict-sidebar.c
@@ -3,16 +3,16 @@
  * Copyright (C) 2006  Emmanuele Bassi <ebassi@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  *
