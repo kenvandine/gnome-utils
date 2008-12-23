4a62404d2aa352d424a4bc55ff2c1a3ffcb9f002
Cosimo Cecchi <cosimoc@src.gnome.org>
2008-12-23 13:26:51 +0000

    Fix project name typo.
    svn path=/trunk/; revision=8275

--- logview/logview-about.h
+++ logview/logview-about.h
@@ -54,7 +54,7 @@ static const char * logview_about_license[] = {
      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
      "GNU General Public License for more details."),
   N_("You should have received a copy of the GNU General Public License "
-     "along with the GNOME Web Browser; if not, write to the Free Software Foundation, Inc., "
+     "along with the Log Viewer; if not, write to the Free Software Foundation, Inc., "
      "51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA")
 };
 
